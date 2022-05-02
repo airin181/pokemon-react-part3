3081e0c2c4d704b0041b7a03978e295d72fa765d
airin181 <98608110+airin181@users.noreply.github.com>
2022-05-02 19:11:03 +0200

    BASIC strucutre

---
+++ pokeapi/src/App.js
@@ -0,0 +1,14 @@
+import Main from './components/Main/Main'
+import Header from './components/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pokeapi/src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Card from './Card/Card'
+import axios from 'axios';
+
+
+class Main extends Component {
+  render() {
+    return <div>Main</div>;
+  }
+}
+
+export default Main;
