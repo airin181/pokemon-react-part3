10ab1c1e67c93b8b7a774b659e06c605de8c398f
airin181 <98608110+airin181@users.noreply.github.com>
2022-05-04 13:34:05 +0200

    ADD: some css

--- pokeapi/src/components/Header/Header.jsx
+++ pokeapi/src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import logo from './../../assets/logo.svg'
 
 class Header extends Component {
   render() {
-    return <div>
-      <h1>POKEAPI</h1>
+    return <div className='header'>
+
+     
+      <img src={logo} alt="logo pokemon" />
       <h3>Busca tu favorito y hazte con todos</h3>
+      
     </div>;
   }
 }
 
 export default Header;
+
+
