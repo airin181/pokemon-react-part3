6722b991459abf15e9361a0ef7e34aa205fbee2d
airin181 <98608110+airin181@users.noreply.github.com>
2022-05-06 13:39:52 +0200

    ADD: debouncing hook

--- pokeapi/src/components/Main/Main.jsx
+++ pokeapi/src/components/Main/Main.jsx
@@ -1,76 +1,98 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import PokeList from './PokeList/PokeList';
-import { useDebounce } from 'use-debounce';
+import Detail from './Detail/Detail';
+import Form from './Form/Form';
+import Search from './Search/Search';
+
+import {Route, Routes} from 'react-router-dom';
+import { useDebounce } from "use-debounce";
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 
 
 const Main = () => {
 
   //Llamamos a la función useState con valor inicial del estado falsy por defecto y nos devuelve dos constantes en un array. El primero es el estado actual y el segundo es el método para actualizar ese estado. 
+  const [value, setValue] = useState(""); // Para guardar el pokemon que se busca en el input
+  const [pokemon, setPokemon] = useState([]); // estado que tendrá un array con pokemon buscados
+  const [debouncedValue] = useDebounce(value, 1000) // valor de texto con retardo
 
-  
+  const [repeated, setRepeated] = useState(false) //comprobar si se escribe uno repe
+  const [notFound, setNotFound] = useState(false)
 
-  const [value, setValue] = useState(""); // Para guardar el dato a buscar
-  const [pokemon, setPokemon] = useState([]); // Para guardar los pokemon
-
-  //para evitar que en cada tecleado haga una búsqueda y la haga cuando pase el tiempo que le indiquemos
-  const debouncedValue = useDebounce(value, 500);
-
-
+  //petición http 
   useEffect(() => {
 
-    if (value !== "") {
-      async function axiosPokemon() {
+    async function getPokemon() {
+      try {
+        //method every --> aplica una FUNCIÓN por cada elemento del array y devuelve true o false
+        //A cada pokemon del array vas a verificar que no tiene el mismo valor que lo que le entra por debouncedValue. Es decir, que no tenga el mismo nombre. Si se repite, no entra.
 
-        try {
-          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${value}`);
+        if (debouncedValue && pokemon.every(e => e.name !== debouncedValue)) {
+
+          setNotFound(false) 
+        
+          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${debouncedValue}`);
           const result = response.data;
-          setPokemon([...pokemon,result]); 
-         
           
-
-        } catch (e) {
-          setPokemon([]) 
+          setPokemon([result,...pokemon]);
+          setRepeated(false)
+        
+        } 
+        
+        else if (debouncedValue && pokemon.some(e => e.name === debouncedValue)){
+          setRepeated(true);
+          setNotFound(false) 
+          
         }
+
+      } catch (e) {
+        setNotFound(true) 
+        setRepeated(false)
       }
-      axiosPokemon();
-    }
-    //eslint-disable-next-line
-  }, [value]); //cada vez que cambie value (estado) se vuelve a lanzar useEffect()
+    } 
+    getPokemon()
+  }, [debouncedValue]);
 
-  //aqui guardo el valor del input del pokemon que se busca
-  const onChangeHandler = e => {
-    e.preventDefault();
-    const newPokemon = e.target.search.value
-    setValue(newPokemon)
-    e.target.search.value = ""
-  };
 
+  //_________función de eliminar pokemon que enviamos por props a pokelist
   const removePokemon = (i) => {
-    //recibe la posicion de entrada y busca en el array de datos y lo borras
-    //FILTER: filtra un array, elimina el que cumpla una condición (posición i) y devuelve un nuevo array (sin ese elemento con posición i)
   const remainingPokemon = pokemon.filter((poke, j)=> i!== j);
   setPokemon(remainingPokemon);
 }   
 
 
+  //_________función para actualizar estado con el valor introducido en el input
+  const handleChange = e => {
+  e.preventDefault();
+  setValue(e.target.value.toLowerCase())
+};
+
+
   return (
     <section className='main'>
 
-      <div className='searcher'>
-        <h1>Completa la Pokédex buscando tu pokemon por su nombre o usando su número de la Pokédex Nacional!</h1>
-        <form onChange={onChangeHandler} className="form-searcher">
-          <input name="search" type="text" placeholder='Busca aquí tu pokemon'/>
-        </form>
-      </div>
 
-        <PokeList value={pokemon} remove={(i) => removePokemon(i)}></PokeList>  
+    <div className='searcher'>
+      <h1>Completa la Pokédex buscando tu pokemon por su nombre o usando su número de la Pokédex!</h1>
+      <input placeholder='Busca aquí tu pokemon' onChange={handleChange}/>   
+    </div>
+
+      {repeated ? <Stack><Alert severity="warning">¡No puede haber pokemon repetidos! Inténtalo de nuevo 😃</Alert></Stack> : "" }
+      {notFound ? <Stack><Alert severity="error">¡No se ha encontrado el pokemon que indicas! ¿Estás seguro de que lo has escrito bien? 😃</Alert></Stack> : "" }
+     <Routes>
+      <Route element={<PokeList value={pokemon} key = {uuidv4()} remove={(i) => removePokemon(i)}/>} path='/'/>
+      <Route element={<Form/>} path='/form'/>
+      <Route element={<Search/>} path='/search'/>
+      <Route element={<Detail/>} path='/pokemon/:id'/>
+    </Routes> 
 
     </section>
   )
-
 };
-
 
 export default Main
